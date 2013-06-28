20ec56e637a1cf279ccf6b13fd202df175099ee0
Horst Schneider <dieterdervermieter@gmail.com>
2013-06-28 14:08:53 +0200

    added some sample code for random variables in uniform distributions

---
+++ random_variable.py
@@ -0,0 +1,23 @@
+''' 
+a random variable is a function that distributes 
+all values in the universe U to a space of V
+'''
+
+def uniform_distribution(universe, random_variable):
+	results = [random_variable(u) for u in universe]
+	v = {}
+
+	for result in results:
+		v[result] = v[result] + 1 if result in v else 1
+
+	for key in v.iterkeys():
+		print key, ":", float(v[key])  / len(results)
+
+if __name__ == "__main__":
+	universe = [1,2,3,4,5,6]
+	random_variable = lambda x: "even" if x % 2 == 0 else "odd"
+	uniform_distribution(universe, random_variable)
+	print "\n"
+	uniform_distribution(universe, lambda x: x ** 2)
+	print "\n"
+	uniform_distribution(universe, lambda x: x / 2)
